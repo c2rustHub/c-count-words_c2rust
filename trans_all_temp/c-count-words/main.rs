cb9d166d75da8058d1bddc8509148383abc15c6c
c2rust <root@localhost.localdomain>
2024-12-12 16:30:59 +0800

    translate from cpp to rust

--- trans_all_temp/c-count-words/main.rs
+++ trans_all_temp/c-count-words/main.rs
@@ -1,14 +1,15 @@
 fn get_input_type(c: char) -> i32 {
     if c.is_alphabetic() {
-        return 1;
+        1
+    } else {
+        0
     }
-    0
 }
 
 fn main() {
     let str = "Don't ask what your country can do for you, but ask what you can do for your country.";
     let mut state = 0; // init state
-    let mut input = 0; // 0: space		1: alpha
+    let mut input = 0; // 0: space 1: alpha
     let mut counter = 0;
 
     println!("hello, count how many words in string:");
